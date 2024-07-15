47e984d5495209cd8e6acecde2c2c1bdff97e4cd
Emmanuelle Gouillart <emmanuelle.gouillart@gmail.com>
2024-07-15 21:02:57 +0200

    add table with names of polling stations

--- raw_data/extract_circo.py
+++ raw_data/extract_circo.py
@@ -9,3 +9,15 @@ df_circo.to_excel('legislatives2024_t2_9213.xlsx')
 df = pd.read_excel('resultats-provisoires-par-bureau-de-votevmn.xlsx')
 df_circo = df[df['Code commune'].isin(['92019', '92014', '92071', '92002'])]
 df_circo.to_excel('legislatives2024_t1_9213.xlsx')
+
+# Data from https://public.opendatasoft.com/explore/dataset/elections-france-legislatives-2022-2nd-tour-par-bureau-de-vote/export/?flg=fr-fr
+df = pd.read_excel('elections-france-legislatives-2022-2nd-tour-par-bureau-de-vote.xlsx')
+df_circo = df[df['Code de la commune'].isin(['92019', '92014', '92071', '92002'])]
+df_circo['id_bv'] = df_circo['Code de la commune'] + '_' + df_circo['Code du b.vote']
+df_circo.to_excel('legislatives2022_t2_9213.xlsx')
+
+dict_bv_names = dict(zip(df_circo.id_bv,
+                         df_circo['Libellé du bureau de vote']))
+df_bv = pd.DataFrame.from_dict(dict_bv_names, orient='index',
+                               columns=['names'])
+df_bv.to_excel('noms_bureaux_votes.xlsx')
