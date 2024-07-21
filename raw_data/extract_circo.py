583c4478d62994bd03305b36aa897fed62fe06b0
Emmanuelle Gouillart <emmanuelle.gouillart@gmail.com>
2024-07-21 09:21:34 +0200

    add columns in excel files to simplify

--- raw_data/extract_circo.py
+++ raw_data/extract_circo.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
-# Data from https://www.data.gouv.fr/fr/datasets/elections-legislatives-des-30-juin-et-7-juillet-2024-resultats-definitifs-du-2nd-tour/
-df = pd.read_excel('resultats-definitifs-par-bureau-de-vote.xlsx')
-df_circo = df[df['Code commune'].isin(['92019', '92014', '92071', '92002'])]
-df_circo.to_excel('legislatives2024_t2_9213.xlsx')
+def format_series(s):
+    """
+    Replace '35,2%' by 35.2 (type float) to manipulate numbers instead of str
+    """
+    return float(s[:-1].replace(',', '.'))
 
-# Data from https://www.data.gouv.fr/fr/datasets/elections-legislatives-des-30-juin-et-7-juillet-2024-resultats-definitifs-du-1er-tour/
-df = pd.read_excel('resultats-provisoires-par-bureau-de-votevmn.xlsx')
-df_circo = df[df['Code commune'].isin(['92019', '92014', '92071', '92002'])]
-df_circo.to_excel('legislatives2024_t1_9213.xlsx')
 
 # Data from https://public.opendatasoft.com/explore/dataset/elections-france-legislatives-2022-2nd-tour-par-bureau-de-vote/export/?flg=fr-fr
-df = pd.read_excel('elections-france-legislatives-2022-2nd-tour-par-bureau-de-vote.xlsx')
+df = pd.read_excel('original_data/elections-france-legislatives-2022-2nd-tour-par-bureau-de-vote.xlsx')
 df_circo = df[df['Code de la commune'].isin(['92019', '92014', '92071', '92002'])]
 df_circo['id_bv'] = df_circo['Code de la commune'] + '_' + df_circo['Code du b.vote']
 df_circo.to_excel('legislatives2022_t2_9213.xlsx')
@@ -22,6 +19,23 @@ df_bv = pd.DataFrame.from_dict(dict_bv_names, orient='index',
                                columns=['names'])
 df_bv.to_excel('noms_bureaux_votes.xlsx')
 
+
+# Data from https://www.data.gouv.fr/fr/datasets/elections-legislatives-des-30-juin-et-7-juillet-2024-resultats-definitifs-du-2nd-tour/
+df = pd.read_excel('original_data/resultats-definitifs-par-bureau-de-vote.xlsx')
+df_circo = df[df['Code commune'].isin(['92019', '92014', '92071', '92002'])]
+df_circo.to_excel('legislatives2024_t2_9213.xlsx')
+
+# Data from https://www.data.gouv.fr/fr/datasets/elections-legislatives-des-30-juin-et-7-juillet-2024-resultats-definitifs-du-1er-tour/
+df = pd.read_excel('original_data/resultats-provisoires-par-bureau-de-votevmn.xlsx')
+df_circo = df[df['Code commune'].isin(['92019', '92014', '92071', '92002'])]
+df_circo['Gaillard'] = df_circo['% Voix/exprimés 2'].map(format_series)
+df_circo['Bregeon'] = df_circo['% Voix/exprimés 4'].map(format_series)
+df_circo['Isnard'] = df_circo['% Voix/exprimés 3'].map(format_series)
+df_circo['Yvars'] = df_circo['% Voix/exprimés 6'].map(format_series)
+df_circo['Participation'] = df_circo['% Votants'].map(format_series)
+df_circo['id_bv'] = df_circo['Code commune'].astype(str) + '_' + df_circo['Code BV'].astype(str)
+df_circo['nomBureauVote'] = df_circo['id_bv'].map(dict_bv_names) 
+df_circo.to_excel('legislatives2024_t1_9213.xlsx')
 
 # Departementales
 # Data from https://www.data.gouv.fr/fr/datasets/elections-departementales-2021-resultats-du-1er-tour/
