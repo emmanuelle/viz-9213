17634ebef31eb53392394da84f2b271e3abd5030
Emmanuelle Gouillart <emmanuelle.gouillart@gmail.com>
2024-07-22 23:51:14 +0200

    add data source for carreaux

--- raw_data/extract_carreaux_circo.py
+++ raw_data/extract_carreaux_circo.py
@@ -1,5 +1,8 @@
 import geopandas as gpd
 
+
+# Original data from https://www.insee.fr/fr/statistiques/7655475?sommaire=7655515
+
 carreaux = gpd.read_file('original_data/carreaux_200m_met.gpkg')
 carreaux_circo = carreaux[carreaux['lcog_geo'].isin(['92019', '92014', '92071', '92002'])]
 
