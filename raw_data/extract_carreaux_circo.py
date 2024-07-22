4ff424109244f929798f7cb6afc59f2a9d89dbeb
Emmanuelle Gouillart <emmanuelle.gouillart@gmail.com>
2024-07-23 00:26:34 +0200

    convert coordinates of carreaux

--- raw_data/extract_carreaux_circo.py
+++ raw_data/extract_carreaux_circo.py
@@ -8,3 +8,23 @@ carreaux_circo = carreaux[carreaux['lcog_geo'].isin(['92019', '92014', '92071', 
 
 carreaux_circo.to_file("carreaux.geojson", driver='GeoJSON')
 carreaux_circo.to_file("carreaux.gpkg", driver='GPKG')
+
+import pyproj
+
+from shapely.geometry import Polygon
+from shapely.ops import transform
+
+
+inCRS = pyproj.CRS('EPSG:2154') # Lambert 93 / RGF 93
+outCRS = pyproj.CRS('EPSG:4326') # Latitude longitude
+
+project = pyproj.Transformer.from_crs(inCRS, outCRS, always_xy=True).transform
+
+def transform_poly(poly):
+    return transform(project, poly)
+
+carreaux_circo['other_geometry'] = carreaux_circo['geometry'].map(transform_poly)
+
+carreaux_circo['geometry'] = carreaux_circo['other_geometry']
+carreaux_circo.drop(columns=['other_geometry']).to_file('carreaux_geo.geojson', driver='GeoJSON')
+carreaux_circo.drop(columns=['other_geometry']).to_file('carreaux_geo.gpkg', driver='GPKG')
